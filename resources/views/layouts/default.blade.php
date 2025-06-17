12ae117071f10844f152f65cea94210de742c300
zeed2411 <xzidane24@gmail.com>
2025-06-17 09:22:55 +0700

    Pindahkan app.css ke folder resources/css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,6 +7,8 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link href="{{ asset("assets\css\bootstrap.min.css") }}" rel="stylesheet">
+    
+
     @yield("style")
   </head>
   <body class="d-flex flex-column h-100">
